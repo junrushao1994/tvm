da78805772411d052d1a7dc69a88c38b9ff40651
Junru Shao <junrushao1994@gmail.com>
2018-12-08 17:44:38 -0500

    Let's deprecate this branch

--- topi/recipe/rnn/rnns.py
+++ topi/recipe/rnn/rnns.py
@@ -5,16 +5,14 @@ import numpy as np
 
 
 # Quick knobs
-TASK="lstm"
+TASK="rnn"
 USE_MANUAL_CODE = False
-PERSIST_KERNEL = True
-DETECT_GLOBAL_BARRIER = PERSIST_KERNEL
-SKIP_CHECK = False
-UNROLL_WLOAD = True
-NUM_SM = 60   # 80 streaming processors in V100
+DETECT_GLOBAL_BARRIER = True
+UNROLL = True
+NUM_SM = 80   # 80 streaming processors in V100
 
-NUM_THREAD_Y = 8
-NUM_THREAD_X = 16 * 3 // 2
+NUM_THREAD_Y = 32
+NUM_THREAD_X = 4
 
 
 @tvm.register_func
@@ -35,153 +33,219 @@ def tvm_callback_cuda_postproc(code):
   return code
 
 
-def _linear(x, w, b, name):
-  # x: [seq_len, batch_size, input_dim]
-  # w: [num_gemm, num_hidden, input_dim]
-  # b: None or [num_gemm, num_hidden]
-  # k: IterVar(min=0, extent=input_dim)
-  assert w is not None
-  seq_len = x.shape[0]
-  batch_size = x.shape[1]
-  input_dim = x.shape[2]
-  num_gemm = w.shape[0]
-  num_hidden = w.shape[1]
+def _input(seq_len, batch_size, input_dim, name, **kwargs):
+  x = tvm.placeholder((seq_len, batch_size, input_dim), name=name)
+  def sch(s, scan, readers, n_tx, n_ty, tid_x, tid_y, **kwargs):
+    # fetch inputs
+    xL = s.cache_read(x, "shared", readers=readers)
+    s[xL].compute_at(s[scan], s[scan].op.scan_axis)
+    # split `nc` into threadIdx.y * threadIdx.x
+    _, n, c = s[xL].op.axis
+    nc = s[xL].fuse(n, c)
+    _, xi = s[xL].split(nc, factor=n_ty * n_tx)
+    ty, xi = s[xL].split(xi, nparts=n_ty)
+    tx, _ = s[xL].split(xi, nparts=n_tx)
+    # bind with threads
+    s[xL].bind(ty, tid_y)
+    s[xL].bind(tx, tid_x)
+    s[xL].double_buffer()
+  return (x, ), sch
 
-  k = tvm.reduce_axis((0, input_dim), name=name + "_k")
-  if b is None:
-    wx = tvm.compute((seq_len, batch_size, num_gemm, num_hidden),
-                      lambda t, n, a, c: tvm.sum(w[a, c, k] * x[t - 1, n, k], axis=k),
-                      name=name + "_w")
-  else:
-    wx = tvm.compute((seq_len, batch_size, num_gemm, num_hidden),
-                      lambda t, n, a, c: tvm.sum(w[a, c, k] * x[t - 1, n, k] + b[a][c], axis=k),
-                      name=name + "_w")
-  return wx, k
+
+def _state(seq_len, batch_size, num_hidden, name, **kwargs):
+  state = tvm.placeholder((seq_len, batch_size, num_hidden), name=name)
+  s_init = tvm.compute((1, batch_size, num_hidden), lambda _, n, c: 0.0, name="init_" + name)
+  def sch(s, scan, readers, **kwargs):
+    def _state(n_ty, n_tx, tid_y, tid_x, **kwargs):
+      # fetching the state
+      sS = s.cache_read(state, "shared", readers=readers)
+      s[sS].compute_at(s[scan], s[scan].op.scan_axis)
+      # split `nc` into threadIdx.y * threadIdx.x
+      _, n, c = s[sS].op.axis
+      nc = s[sS].fuse(n, c)
+      _, xi = s[sS].split(nc, factor=n_ty * n_tx)
+      ty, xi = s[sS].split(xi, nparts=n_ty)
+      tx, _ = s[sS].split(xi, nparts=n_tx)
+      # bind with threads
+      s[sS].bind(ty, tid_y)
+      s[sS].bind(tx, tid_x)
+      s[sS].double_buffer()
+    def _s_init(n_bx, n_tx, bid_x, tid_x, tid_y, **kwargs):
+      # split `nc` into blockIdx.x * threadIdx.x
+      _, n, c = s[s_init].op.axis
+      nc = s[s_init].fuse(n, c)
+      _, xi = s[s_init].split(nc, factor=n_bx * n_tx)
+      bx, xi = s[s_init].split(xi, nparts=n_bx)
+      tx, _ = s[s_init].split(xi, nparts=n_tx)
+      # bind with threads
+      s[s_init].bind(bx, bid_x)
+      s[s_init].bind(tx, tid_x)
+      s[s_init].set_store_predicate(tid_y.equal(0))
+    _state(**kwargs)
+    _s_init(**kwargs)
+  return (state, s_init), sch
+
+
+def _linear(x, num_gemm, num_hidden, name, **kwargs):
+  seq_len, batch_size, input_dim = x.shape
+  w = tvm.placeholder((num_gemm, num_hidden, input_dim), name="w_" + name)
+  b = tvm.placeholder((num_gemm, num_hidden), name="b_" + name)
+  k = tvm.reduce_axis((0, input_dim), name="k_" + name)
+  g = tvm.compute((seq_len, batch_size, num_gemm, num_hidden),
+                   lambda t, n, a, c: tvm.sum(x[t - 1, n, k] * w[a, c, k], axis=k),
+                   name="g_" + name)
+  f = tvm.compute((seq_len, batch_size, num_gemm, num_hidden),
+                   lambda t, n, a, c: g[t, n, a, c] + b[a, c],
+                   name=name)
+  def sch(s, scan, **kwargs):
+    def _w(bid_x, tid_y, tid_x, n_bx, n_ty, n_tx, **kwargs):
+      # w[a, c, i] => (blockIdx.x, *, threadIdx.x)``
+      wS = s.cache_read(w, "shared", readers=[g])
+      s[wS].compute_at(s[scan], tid_x)
+      a, c, i = s[wS].op.axis
+      ac = s[wS].fuse(a, c)
+      bx, _ = s[wS].split(ac, nparts=n_bx)
+      _, tx = s[wS].split(i, factor=n_tx)
+      s[wS].bind(bx, bid_x)
+      s[wS].bind(tx, tid_x)
+      s[wS].set_store_predicate(tid_y.equal(0))
+    def _b(bid_x, tid_y, tid_x, n_bx, n_ty, n_tx, **kwargs):
+      # b[a, c] => (blockIdx.x, *, threadIdx.x)
+      bS = s.cache_read(b, "shared", readers=[f])
+      s[bS].compute_at(s[scan], tid_x)
+      a, c = s[bS].op.axis
+      ac = s[bS].fuse(a, c)
+      bx, _ = s[bS].split(ac, nparts=n_bx)
+      s[bS].bind(bx, bid_x)
+      s[bS].set_store_predicate(tvm.all(tid_x.equal(0), tid_y.equal(0)))
+    def _mm(bid_x, tid_y, tid_x, n_bx, n_ty, n_tx, **kwargs):
+      # inline the bias add
+      s[f].compute_inline()
+      # do reduction on the last dimension
+      k, = s[g].op.reduce_axis
+      ko, _ = s[g].split(k, nparts=n_ty)
+      rf = s.rfactor(g, ko)
+      k, = s[g].op.reduce_axis
+      s[rf].compute_at(s[g], k)
+      # bind with threads
+      _, _, a, c = s[g].op.axis
+      ac = s[g].fuse(a, c)
+      bx, _ = s[g].split(ac, nparts=n_bx)
+      ty, = s[g].op.reduce_axis
+      s[g].bind(bx, bid_x)
+      s[g].bind(ty, tid_y)
+      s[g].set_store_predicate(tid_x.equal(0))
+    _w(**kwargs)
+    _b(**kwargs)
+    _mm(**kwargs)
+  return (f, g, w, b), sch
+
+
+def _update_state(x, seq_len, batch_size, num_hidden, name, **kwargs):
+  u = tvm.compute((seq_len, batch_size, num_hidden), lambda t, n, c: x[t, n, c], name=name)
+  def sch(s, n_bx, n_tx, bid_x, tid_x, tid_y, **kwargs):
+    s[x].compute_inline()
+    _, _, c = s[u].op.axis
+    _, xi = s[u].split(c, factor=n_bx * n_tx)
+    bx, xi = s[u].split(xi, nparts=n_bx)
+    tx, _ = s[u].split(xi, nparts=n_tx)
+    # bind with threads
+    s[u].bind(bx, bid_x)
+    s[u].bind(tx, tid_x)
+    s[u].set_store_predicate(tid_y.equal(0))
+  return (u, ), sch
 
 
 def vanilla(n_seq_len=128, n_num_hidden=128, n_input_dim=128, n_batch_size=1):
-  # This vanilla RNN includes input projection and all bias terms, should exactly match that of cuDNN
-  seq_len = tvm.var("seq_len")
-  batch_size = tvm.var("batch_size")
-  num_hidden = tvm.var("num_hidden")
-  input_dim = tvm.var("input_dim")
-  # Define the input
-  x = tvm.placeholder((seq_len, batch_size, input_dim), name="x")
+  n_seq_len += 1
+  seq_len = tvm.convert(n_seq_len)
+  batch_size = tvm.convert(n_batch_size)
+  num_hidden = tvm.convert(n_num_hidden)
+  input_dim = tvm.convert(n_input_dim)
+  # batch_size = tvm.var("batch_size")
+  # num_hidden = tvm.var("num_hidden")
+  # input_dim = tvm.var("input_dim")
+  config = {
+    "seq_len": seq_len,
+    "batch_size": batch_size,
+    "num_hidden": num_hidden,
+    "input_dim": input_dim,
+    "num_gemm": 1,
+  }
   # Define weight and bias for input projection and hidden state transformation
-  w_i2h = tvm.placeholder((1, num_hidden, input_dim), name="w_i2h")
-  b_i2h = tvm.placeholder((1, num_hidden), name="b_i2h")
-  w_h2h = tvm.placeholder((1, num_hidden, num_hidden), name="w_h2h")
-  b_h2h = tvm.placeholder((1, num_hidden), name="b_h2h")
-  # Define hidden state and cell state
-  s_h = tvm.placeholder((seq_len, batch_size, num_hidden), name="s_h")
-  s_h_init = tvm.compute((1, batch_size, num_hidden), lambda *i: 0.0, name="s_h_init")
-  # Do the transformation, the bias for input projection can be fused into that of hidden transformation
-  bias = tvm.compute((1, num_hidden), lambda *i: b_i2h(*i) + b_h2h(*i), name="bias")
-  l_i2h, k_i2h = _linear(x, w_i2h, None, name="l_i2h")
-  l_h2h, k_h2h = _linear(s_h, w_h2h, bias, name="l_h2h")
-  # Sum up the transformed inputs and previous hidden states
-  g = tvm.compute((seq_len, batch_size, 1, num_hidden), lambda *i: l_i2h(*i) + l_h2h(*i), name="g")
-  # Computation inside an LSTM cell
-  n_h = tvm.compute((seq_len, batch_size, num_hidden), lambda t, n, c: tvm.tanh(g[t, n, 0, c]), name="g_i")
+  (x_i, ), sch_x_i = _input(name="x_i", **config)
+  (s_h, init_s_h), sch_s_h = _state(name="s_h", **config)
+  (l_i2h, g_i2h, w_i2h, b_i2h), sch_i2h = _linear(x_i, name="l_i2h", **config)
+  (l_h2h, g_h2h, w_h2h, b_h2h), sch_h2h = _linear(s_h, name="l_h2h", **config)
+  # Computation inside the RNN cell
+  n_h = tvm.compute((seq_len, batch_size, num_hidden), lambda t, n, c: tvm.tanh(l_i2h[t, n, 0, c] + l_h2h[t, n, 0, c]), name="n_h")
   # Define update rules explicitly
-  u_h = tvm.compute((seq_len, batch_size, num_hidden), lambda *i: n_h(*i), name="u_h")
+  (u_h, ), sch_u_h = _update_state(n_h, name="u_h", **config)
   # Finally, define the scanning itself
   scan_h = tvm.scan(
-    init=[s_h_init],
-    update=[u_h],
     state_placeholder=[s_h],
-    inputs=[x],
-    name="lstm_scan")
+    init=[init_s_h],
+    update=[u_h],
+    inputs=[x_i],
+    name="scan_h")
   # schedule
   s = tvm.create_schedule([scan_h.op])
+  sch_cfg = {
+    "n_bx": NUM_SM,
+    "n_tx": NUM_THREAD_X,
+    "n_ty": NUM_THREAD_Y,
+    "bid_x": tvm.thread_axis((0, NUM_SM), "blockIdx.x"),
+    "tid_x": tvm.thread_axis((0, NUM_THREAD_X), "threadIdx.x"),
+    "tid_y": tvm.thread_axis((0, NUM_THREAD_Y), "threadIdx.y"),
+  }
+  # some collections and auxiliary definitions
+  def lower(simple_mode=True):
+    return tvm.lower(s, [w_i2h, b_i2h, w_h2h, b_h2h, x_i, scan_h], simple_mode=simple_mode)
+  # do the scheduling
+  s[scan_h.op].env_threads([sch_cfg["bid_x"], sch_cfg["tid_y"], sch_cfg["tid_x"]])
+  sch_x_i(s, scan=scan_h, readers=[g_i2h], **sch_cfg)
+  sch_s_h(s, scan=scan_h, readers=[g_h2h], **sch_cfg)
+  sch_i2h(s, scan=scan_h, **sch_cfg)
+  sch_h2h(s, scan=scan_h, **sch_cfg)
+  sch_u_h(s, **sch_cfg)
 
-
-def gru(n_seq_len=128, n_num_hidden=128, n_input_dim=128, n_batch_size=1):
-  # This GRU includes input projection and all bias terms, should exactly match that of cuDNN
-  seq_len = tvm.var("seq_len")
-  batch_size = tvm.var("batch_size")
-  num_hidden = tvm.var("num_hidden")
-  input_dim = tvm.var("input_dim")
-  # Define the input
-  x = tvm.placeholder((seq_len, batch_size, input_dim), name="x")
-  # Define weight and bias for input projection and hidden state transformation
-  w_i2h = tvm.placeholder((3, num_hidden, input_dim), name="w_i2h")
-  b_i2h = tvm.placeholder((3, num_hidden), name="b_h2h")
-  w_h2h = tvm.placeholder((3, num_hidden, num_hidden), name="w_h2h")
-  b_h2h = tvm.placeholder((3, num_hidden), name="b_h2h")
-  # Define hidden state and cell state
-  s_h = tvm.placeholder((seq_len, batch_size, num_hidden), name="s_h")
-  s_h_init = tvm.compute((1, batch_size, num_hidden), lambda *i: 0.0, name="s_h_init")
-  # Do the transformation, the bias for input projection can be fused into that of hidden transformation
-  l_i2h, k_i2h = _linear(x, w_i2h, b_i2h, name="l_i2h")
-  l_h2h, k_h2h = _linear(s_h, w_h2h, b_h2h, name="l_h2h")
-  # Computation inside an GRU cell
-  g_r = tvm.compute((seq_len, batch_size, num_hidden), lambda t, n, c: l_i2h[t, n, 0, c] + l_h2h[t, n, 0, c], name="g_r")
-  g_i = tvm.compute((seq_len, batch_size, num_hidden), lambda t, n, c: l_i2h[t, n, 1, c] + l_h2h[t, n, 1, c], name="g_i")
-  g_h = tvm.compute((seq_len, batch_size, num_hidden), lambda t, n, c: l_i2h[t, n, 2, c] + g_r[t, n, c] * l_h2h[t, n, 2, c], name="g_h")
-  n_h = tvm.compute((seq_len, batch_size, num_hidden), lambda *i: (1.0 - g_i(*i)) * g_h(*i) + g_i(*i) * s_h(*i), name="n_h")
-  # Define update rules explicitly
-  u_h = tvm.compute((seq_len, batch_size, num_hidden), lambda *i: n_h(*i), name="u_h")
-  # Finally, define the scanning itself
-  scan_h = tvm.scan(
-    init=[s_h_init],
-    update=[u_h],
-    state_placeholder=[s_h],
-    inputs=[x],
-    name="gru_scan")
-  # schedule
-  s = tvm.create_schedule(scan_h.op)
-
-
-def lstm(n_seq_len=128, n_num_hidden=128, n_input_dim=128, n_batch_size=1):
-  # This LSTM includes input projection and all bias terms, should exactly match that of cuDNN
-  seq_len = tvm.var("seq_len")
-  batch_size = tvm.var("batch_size")
-  num_hidden = tvm.var("num_hidden")
-  input_dim = tvm.var("input_dim")
-  # Define the input
-  x = tvm.placeholder((seq_len, batch_size, input_dim), name="x")
-  # Define weight and bias for input projection and hidden state transformation
-  w_i2h = tvm.placeholder((4, num_hidden, input_dim), name="w_i2h")
-  b_i2h = tvm.placeholder((4, num_hidden), name="b_i2h")
-  w_h2h = tvm.placeholder((4, num_hidden, num_hidden), name="w_h2h")
-  b_h2h = tvm.placeholder((4, num_hidden), name="b_h2h")
-  # Define hidden state and cell state
-  s_h = tvm.placeholder((seq_len, batch_size, num_hidden), name="s_h")
-  s_c = tvm.placeholder((seq_len, batch_size, num_hidden), name="s_c")
-  s_h_init = tvm.compute((1, batch_size, num_hidden), lambda *i: 0.0, name="s_h_init")
-  s_c_init = tvm.compute((1, batch_size, num_hidden), lambda *i: 0.0, name="s_c_init")
-  # Do the transformation, the bias for input projection can be fused into that of hidden transformation
-  bias = tvm.compute((1, num_hidden), lambda *i: b_i2h(*i) + b_h2h(*i), name="bias")
-  l_i2h, k_i2h = _linear(x, w_i2h, None, name="l_i2h")
-  l_h2h, k_h2h = _linear(s_h, w_h2h, bias, name="l_h2h")
-  # Sum up the transformed inputs and previous hidden states
-  g = tvm.compute((seq_len, batch_size, 4, num_hidden), lambda *i: l_i2h(*i) + l_h2h(*i), name="g")
-  # Computation inside an LSTM cell
-  g_i = tvm.compute((seq_len, batch_size, num_hidden), lambda t, n, c: tvm.sigmoid(g[t, n, 0, c]), name="g_i")
-  g_f = tvm.compute((seq_len, batch_size, num_hidden), lambda t, n, c: tvm.sigmoid(g[t, n, 1, c]), name="g_f")
-  g_c = tvm.compute((seq_len, batch_size, num_hidden), lambda t, n, c: tvm.tanh   (g[t, n, 2, c]), name="g_c")
-  g_o = tvm.compute((seq_len, batch_size, num_hidden), lambda t, n, c: tvm.sigmoid(g[t, n, 3, c]), name="g_o")
-  n_c = tvm.compute((seq_len, batch_size, num_hidden), lambda t, n, c: g_f[t, n, c] * s_c[t - 1, n, c] + g_i[t, n, c] * g_c[t, n, c], name="n_c")
-  n_h = tvm.compute((seq_len, batch_size, num_hidden), lambda t, n, c: g_o[t, n, c] * tvm.tanh(n_c[t, n, c]), name="n_h")
-  # Define update rules explicitly
-  u_h = tvm.compute((seq_len, batch_size, num_hidden), lambda *i: n_h(*i), name="u_h")
-  u_c = tvm.compute((seq_len, batch_size, num_hidden), lambda *i: n_c(*i), name="u_c")
-  # Finally, define the scanning itself
-  scan_h, scan_c = tvm.scan(
-      init=[s_h_init, s_c_init],
-      update=[u_h, u_c],
-      state_placeholder=[s_h, s_c],
-      inputs=[x],
-      name="lstm_scan")
-  # schedule
-  s = tvm.create_schedule([scan_h.op, scan_c.op])
+  def check_device(target="cuda"):
+    assert target == "cuda"
+    frnn = tvm.build(s, [w_i2h, b_i2h, w_h2h, b_h2h, x_i, scan_h], target)
+    ctx = tvm.gpu(0)
+    # prepare data
+    w_i2h_np = np.random.normal(size=(1, n_num_hidden, n_input_dim)).astype("float32")
+    w_h2h_np = np.random.normal(size=(1, n_num_hidden, n_input_dim)).astype("float32")
+    b_i2h_np = np.random.normal(size=(1, n_num_hidden)).astype("float32")
+    b_h2h_np = np.random.normal(size=(1, n_num_hidden)).astype("float32")
+    x_i_np = np.random.normal(size=(n_seq_len, n_batch_size, n_input_dim)).astype("float32")
+    s_h_np = np.random.normal(size=(n_seq_len, n_batch_size, n_num_hidden)).astype("float32")
+    # convert to NDArray
+    w_i2h_nd = tvm.nd.array(w_i2h_np, ctx)
+    w_h2h_nd = tvm.nd.array(w_h2h_np, ctx)
+    b_i2h_nd = tvm.nd.array(b_i2h_np, ctx)
+    b_h2h_nd = tvm.nd.array(b_h2h_np, ctx)
+    x_i_nd = tvm.nd.array(x_i_np, ctx)
+    s_h_nd = tvm.nd.array(s_h_np, ctx)
+    # launch the kernel.
+    print("Running the kernel...")
+    frnn(w_i2h_nd, b_i2h_nd, w_h2h_nd, b_h2h_nd, x_i_nd, s_h_nd)
+    ctx.sync()
+    # measure time cost of second step.
+    evaluator = frnn.time_evaluator(frnn.entry_name, ctx, 1, repeat=1000)
+    eval_result = evaluator(w_i2h_nd, b_i2h_nd, w_h2h_nd, b_h2h_nd, x_i_nd, s_h_nd)
+    print("Time cost=%g ms" % (eval_result.mean * 1000.0))
+  with tvm.build_config(
+    detect_global_barrier=DETECT_GLOBAL_BARRIER,
+    auto_unroll_max_step=128,
+    unroll_explicit=False):
+    check_device()
 
 
 def main():
   vanilla()
-  gru()
-  lstm()
+  # gru()
+  # lstm()
 
 
 if __name__ == "__main__":
