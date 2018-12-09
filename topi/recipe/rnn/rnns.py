b9796a7138f0d70b9a6596136bc445e5426300c9
Junru Shao <junrushao1994@gmail.com>
2018-12-09 00:01:32 -0500

    At least the lower() looks good

--- topi/recipe/rnn/rnns.py
+++ topi/recipe/rnn/rnns.py
@@ -12,6 +12,7 @@ DETECT_GLOBAL_BARRIER = False
 NUM_SM = 80   # 80 streaming processors in V100
 
 NUM_THREAD_X = 32
+BOUND = True
 
 
 @tvm.register_func
@@ -39,7 +40,8 @@ def _input(seq_len, batch_size, input_dim, name, **kwargs):
     s[xL].compute_at(s[scan], s[scan].op.scan_axis)
     _t, _n, c = s[xL].op.axis
     _co, tx = s[xL].split(c, factor=n_tx)
-    s[xL].bind(tx, tid_x)
+    if BOUND:
+      s[xL].bind(tx, tid_x)
     if DOUBLE_BUFFER:
       s[xL].double_buffer()
   return (x, ), sch
@@ -57,22 +59,69 @@ def _state(seq_len, batch_size, num_hidden, name, **kwargs):
       s[sS].compute_at(s[scan], s[scan].op.scan_axis)
       _t, _n, c = s[sS].op.axis
       _co, tx = s[sS].split(c, factor=n_tx)
-      s[sS].bind(tx, tid_x)
+      if BOUND:
+        s[sS].bind(tx, tid_x)
       if DOUBLE_BUFFER:
         s[sS].double_buffer()
     def _s_init(n_bx, tid_x, bid_x, **kwargs):
       _1, _n, c = s[s_init].op.axis
       bx, _ci = s[s_init].split(c, nparts=n_bx)
-      s[s_init].bind(bx, bid_x)
-      s[s_init].set_store_predicate(tid_x.equal(0))
+      if BOUND:
+        s[s_init].bind(bx, bid_x)
+        s[s_init].set_store_predicate(tid_x.equal(0))
     _state(**kwargs)
     _s_init(**kwargs)
   return (state, s_init), sch
 
 
 def _linear(x, num_gemm, num_hidden, name, **kwargs):
-  # TODO
-  pass
+  seq_len, batch_size, input_dim = x.shape
+  w = tvm.placeholder((num_hidden, num_gemm, input_dim), name="w_" + name)
+  b = tvm.placeholder((num_hidden, num_gemm), name="b_" + name)
+  i = tvm.reduce_axis((0, input_dim), name="k_" + name)
+  g = tvm.compute((seq_len, num_hidden, batch_size, num_gemm),
+                   lambda t, c, n, a: tvm.sum(x[t - 1, n, i] * w[c, a, i], axis=i),
+                   name=name)
+  f = tvm.compute((seq_len, num_hidden, batch_size, num_gemm),
+                   lambda t, c, n, a: g[t, c, n, a] + b[c, a],
+                   name="_bias_" + name)
+  def sch(s, scan, **kwargs):
+    def _w(bid_x, tid_x, n_tx, n_bx, **kwargs):
+      # w[c, a, i]
+      wS = s.cache_read(w, "shared", readers=[g])
+      s[wS].compute_at(s[scan], tid_x)
+      c, _a, i = s[wS].op.axis
+      bx, _ci = s[wS].split(c, nparts=n_bx)
+      tx, _ii = s[wS].split(i, nparts=n_tx)
+      if BOUND:
+        s[wS].bind(bx, bid_x)
+        s[wS].bind(tx, tid_x)
+    def _b(bid_x, tid_x, n_bx, **kwargs):
+      # b[c, a]
+      bS = s.cache_read(b, "shared", readers=[f])
+      s[bS].compute_at(s[scan], tid_x)
+      c, _a = s[bS].op.axis
+      bx, _ci = s[bS].split(c, nparts=n_bx)
+      if BOUND:
+        s[bS].bind(bx, bid_x)
+        s[bS].set_store_predicate(tid_x.equal(0))
+    def _mm(bid_x, tid_x, n_tx, n_bx, **kwargs):
+      s[f].compute_inline()
+      k, = s[g].op.reduce_axis
+      ko, _ki = s[g].split(k, nparts=n_tx)
+      rf = s.rfactor(g, ko)
+      k, = s[g].op.reduce_axis
+      s[rf].compute_at(s[g], k)
+      _t, c, _n, _a = s[g].op.axis
+      bx, _ci = s[g].split(c, nparts=n_bx)
+      if BOUND:
+        tx, = s[g].op.reduce_axis
+        s[g].bind(bx, bid_x)
+        s[g].bind(tx, tid_x)
+    _w(**kwargs)
+    _b(**kwargs)
+    _mm(**kwargs)
+  return (f, g, w, b), sch
 
 
 def _update_state(x, seq_len, batch_size, num_hidden, name, **kwargs):
@@ -80,8 +129,9 @@ def _update_state(x, seq_len, batch_size, num_hidden, name, **kwargs):
   def sch(s, n_bx, bid_x, tid_x, **kwargs):
     s[x].compute_inline()
     _t, _n, c = s[u].op.axis
-    _co, bx = s[u].split(c, factor=n_bx)
-    s[u].bind(bx, bid_x)
+    bx, _ci = s[u].split(c, nparts=n_bx)
+    if BOUND:
+      s[u].bind(bx, bid_x)
     s[u].set_store_predicate(tid_x.equal(0))
   return (u, ), sch
 
@@ -107,8 +157,10 @@ def vanilla(n_seq_len=128, n_num_hidden=128, n_input_dim=128, n_batch_size=1):
   # Define weight and bias for input projection and hidden state transformation
   (x_i, ), sch_x_i = _input(name="x_i", **config)
   (s_h, init_s_h), sch_s_h = _state(name="s_h", **config)
+  (l_i2h, g_i2h, w_i2h, b_i2h), sch_i2h = _linear(x_i, name="l_i2h", **config)
+  (l_h2h, g_h2h, w_h2h, b_h2h), sch_h2h = _linear(s_h, name="l_h2h", **config)
   # Computation inside the RNN cell
-  n_h = tvm.compute((seq_len, batch_size, input_dim), lambda t, n, c: x_i[t - 1, n, c] + s_h[t - 1, n, c], name="n_h")
+  n_h = tvm.compute((seq_len, batch_size, num_hidden), lambda t, n, c: tvm.tanh(l_i2h[t, c, n, 0] + l_h2h[t, c, n, 0]), name="n_h")
   # Define update rules explicitly
   (u_h, ), sch_u_h = _update_state(n_h, name="u_h", **config)
   # Finally, define the scanning itself
@@ -128,33 +180,41 @@ def vanilla(n_seq_len=128, n_num_hidden=128, n_input_dim=128, n_batch_size=1):
   }
   # some collections and auxiliary definitions
   def lower(simple_mode=True):
-    return tvm.lower(s, [x_i, scan_h], simple_mode=simple_mode)
+    return tvm.lower(s, [w_i2h, b_i2h, w_h2h, b_h2h, x_i, scan_h], simple_mode=simple_mode)
   s[scan_h.op].env_threads([sch_cfg["bid_x"], sch_cfg["tid_x"]])
   # do the scheduling
-  sch_x_i(s, scan=scan_h, readers=[n_h], **sch_cfg)
-  sch_s_h(s, scan=scan_h, readers=[n_h], **sch_cfg)
+  sch_x_i(s, scan=scan_h, readers=[g_i2h], **sch_cfg)
+  sch_s_h(s, scan=scan_h, readers=[g_h2h], **sch_cfg)
+  sch_i2h(s, scan=scan_h, **sch_cfg)
+  sch_h2h(s, scan=scan_h, **sch_cfg)
   sch_u_h(s, **sch_cfg)
   print(lower())
 
-  def check_device(target="cuda -arch=sm_70 -O3"):
-    print("Compiling the kernel...")
-    frnn = tvm.build(s, [x_i, scan_h], target)
+  def check_device(target="cuda"):
+    assert target == "cuda"
+    frnn = tvm.build(s, [w_i2h, b_i2h, w_h2h, b_h2h, x_i, scan_h], target)
     ctx = tvm.gpu(0)
     # prepare data
-    x_i_np = np.full(fill_value=1,shape=(n_seq_len, n_batch_size, n_input_dim), dtype="float32")
-    s_h_np = np.full(fill_value=0,shape=(n_seq_len, n_batch_size, n_input_dim), dtype="float32")
+    w_i2h_np = np.random.normal(size=(n_num_hidden, 1, n_input_dim)).astype("float32")
+    w_h2h_np = np.random.normal(size=(n_num_hidden, 1, n_input_dim)).astype("float32")
+    b_i2h_np = np.random.normal(size=(n_num_hidden, 1)).astype("float32")
+    b_h2h_np = np.random.normal(size=(n_num_hidden, 1)).astype("float32")
+    x_i_np = np.random.normal(size=(n_seq_len, n_batch_size, n_input_dim)).astype("float32")
+    s_h_np = np.random.normal(size=(n_seq_len, n_batch_size, n_num_hidden)).astype("float32")
     # convert to NDArray
+    w_i2h_nd = tvm.nd.array(w_i2h_np, ctx)
+    w_h2h_nd = tvm.nd.array(w_h2h_np, ctx)
+    b_i2h_nd = tvm.nd.array(b_i2h_np, ctx)
+    b_h2h_nd = tvm.nd.array(b_h2h_np, ctx)
     x_i_nd = tvm.nd.array(x_i_np, ctx)
     s_h_nd = tvm.nd.array(s_h_np, ctx)
     # launch the kernel.
     print("Running the kernel...")
-    for _ in range(10):
-      frnn(x_i_nd, s_h_nd)
+    frnn(w_i2h_nd, b_i2h_nd, w_h2h_nd, b_h2h_nd, x_i_nd, s_h_nd)
     ctx.sync()
     # measure time cost of second step.
-    print("Benchmarking...")
-    evaluator = frnn.time_evaluator(frnn.entry_name, ctx, 1, repeat=10)
-    eval_result = evaluator(x_i_nd, s_h_nd)
+    evaluator = frnn.time_evaluator(frnn.entry_name, ctx, 1, repeat=1000)
+    eval_result = evaluator(w_i2h_nd, b_i2h_nd, w_h2h_nd, b_h2h_nd, x_i_nd, s_h_nd)
     print("Time cost=%g ms" % (eval_result.mean * 1000.0))
   with tvm.build_config(
     detect_global_barrier=DETECT_GLOBAL_BARRIER,
